6b2652dec3eb31b31859182a3cbe780824cd305c
Fatema-Tuj-Zahura Moumita <fatihamoumita98@gmail.com>
2026-02-22 00:15:56 +0600

    get requested data from body of post method

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // dependencies 
 const http = require('http');
+const { StringDecoder } = require('string_decoder');
 const url = require('url');
 
 
@@ -38,9 +39,23 @@ app.handleReqRes = (req,res) => {
     const method = req.method.toLowerCase();
     const queryStringObject = parsedUrl.query;
     const headersObject = req.headers;
-    console.log(headersObject)
+    // console.log(headersObject)
 
-    res.end("hello world");
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer)=> {
+              realData += decoder.write(buffer);
+    })
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+
+         res.end('Hello World');
+    })
+
+   
 }  
 
 
